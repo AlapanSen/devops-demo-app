fd7be044b4da660481037c2424be5d7b8ad96f4d
I740416 <alapan.sen@sap.com>
2026-08-28 11:24:53 +0530

    2nd commit

--- app/src/main/java/com/sap/training/HelloController.java
+++ app/src/main/java/com/sap/training/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
     @GetMapping("/")
     public String hello() {
-        return "Hello from Alapan This is a devops demo";
+        return "Hello from Alapan Sen  This is a devops demo";
     }
 
     @GetMapping("/health")
